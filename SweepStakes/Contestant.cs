6c73f4380f580088ed4f070bbc4ab6b0232dde40
Brent Solomon <bjsolomon@gmail.com>
2020-06-27 00:48:08 -0400

    Added Contestant Class and UserInterface class

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber;
+
+        public Contestant(string firstName, string lastName, string emailAddress,
+            int registrationNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            RegistrationNumber = registrationNumber;
+        }
+    }
+}
---
+++ SweepStakes/UserInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    public static class UserInterface
+    {
+        //static userinterface methods
+        public static string NewContestantInfo()
+        {
+            string contestantInfo;
+            Console.WriteLine("Enter the following Contestant information:");
+            Console.Write("First Name: ");
+            contestantInfo = Console.ReadLine();
+            Console.Write("Last Name: ");
+            contestantInfo += ',' + Console.ReadLine();
+            Console.Write("Email Address: ");
+            contestantInfo += ',' + Console.ReadLine();
+            Console.WriteLine("Contestant Registration Number: ");
+            contestantInfo += ',' + Console.ReadLine();
+            return contestantInfo;
+        }
+    }
+}
